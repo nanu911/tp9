f7ee4e5a62edc6c3e0a7a977f36cbcfb138bcf5b
nanu911 <nanu.pereyra16@gmail.com>
2025-07-10 20:50:31 -0300

    Update users.routes.js

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const router = express.Router();
 
+
 router.get('/', (req, res) => {
   res.json({ message: "Muestra todos los usuarios" });
 });
 
 router.get('/:id', (req, res) => {
-  res.json({ 
-    message: `GET de users con ID ${req.params.id}` 
+  res.json({
+    message: `Esta es la ruta GET de mi entidad users con el ID ${req.params.id}`
+  });
+});
+
+router.post('/', (req, res) => {
+  res.json({ message: "Usuario creado exitosamente" });
+});
+¡
+router.put('/:id', (req, res) => {
+  res.json({
+    message: `Esta es la ruta PUT de mi entidad users con el ID ${req.params.id}`
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  res.json({
+    message: `Esta es la ruta DELETE de mi entidad users con el ID ${req.params.id}`
   });
 });
 
