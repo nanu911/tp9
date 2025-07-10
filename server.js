9c60c314152bb838fda35eecc10ffb25bc715aee
nanu911 <nanu.pereyra16@gmail.com>
2025-07-10 20:48:54 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const registerRoutes = require('./routes');
 
 app.use(express.json());
 
+// Registrar todas las rutas
 registerRoutes(app);
 
+// Iniciar servidor
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
