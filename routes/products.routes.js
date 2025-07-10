2400bdd37cd3b90a8268fc2a88beecb816b9506a
nanu911 <nanu.pereyra16@gmail.com>
2025-07-10 20:49:54 -0300

    Update products.routes.js

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -6,8 +6,24 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-  res.json({ 
-    message: `GET de products con ID ${req.params.id}` 
+  res.json({
+    message: `Esta es la ruta GET de mi entidad products con el ID ${req.params.id}`
+  });
+});
+
+router.post('/', (req, res) => {
+  res.json({ message: "Producto creado exitosamente" });
+});
+
+router.put('/:id', (req, res) => {
+  res.json({
+    message: `Esta es la ruta PUT de mi entidad products con el ID ${req.params.id}`
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  res.json({
+    message: `Esta es la ruta DELETE de mi entidad products con el ID ${req.params.id}`
   });
 });
 
